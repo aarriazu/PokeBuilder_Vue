5c7ad13c8e6312ca8051c0723e3598dcad25d283
SGomez10 <sgomez@elpuig.xeill.net>
2025-03-28 17:54:35 +0100

    Subtle changes in Login, needed to add Modals for Registrar y Acceder al foro como invitado.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,10 @@
 <template>
   <ion-page>
     <ion-content :fullscreen="true">
+      <!-- Título principal -->
+      <div class="absolute top-0 w-full text-center mt-4">
+        <h1 class="text-7xl font-bold text-black">Pokebuilder</h1>
+      </div>
       <div class="flex items-center justify-center min-h-screen bg-gray-100">
         <!-- Contenedor principal dividido en dos columnas -->
         <div class="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-6xl">
@@ -12,23 +16,27 @@
               type="text" 
               id="userName" 
               name="userName" 
-              class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 mb-4"
+              class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 mb-4 text-white"
             >
             <p class="text-gray-600 mb-2">Contraseña</p>
             <input 
               type="password" 
               id="password" 
               name="password" 
-              class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 mb-4"
+              class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 mb-4 text-white"
             >
             <ion-button 
               router-link="/home/profile" 
-              class="w-full bg-indigo-600 text-white py-2 px-4 rounded-lg hover:bg-indigo-700 transition"
+              class="w-full text-white py-2 px-4 rounded-lg"
             >
               Acceder
             </ion-button>
-            <p class="text-gray-600 mt-4">No tienes cuenta?</p>
-            <p id="login-trigger" class="text-indigo-600 cursor-pointer hover:underline">Creala aquí</p>
+            <p class="text-gray-600 mt-4 flex items-center">
+              ¿No tienes cuenta?
+              <span id="login-trigger" class="text-indigo-600 cursor-pointer hover:underline ml-2">
+                Regístrate aquí
+              </span>
+            </p>
           </div>
 
           <!-- Sección de Actividad Reciente -->
