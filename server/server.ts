df747d694856d6856620f95437938f361b1a48d6
SGomez10 <166744799+SGomez10@users.noreply.github.com>
2025-05-19 20:46:03 +0200

    POST wrong port (TOFIX)

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { MongoClient, ServerApiVersion } from 'mongodb';
 import cors from 'cors';
 import * as db from './DB.js';
+import { insertTorneoPost } from './DB.js';
 const app = express();
 const port = 3000;
 
